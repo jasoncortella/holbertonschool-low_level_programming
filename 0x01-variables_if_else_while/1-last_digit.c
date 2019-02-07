6b6df0bfc788273d4480cf28a236c4c32bb2d894
Jason Cortella <419@holbertonschool.com>
2019-02-07 21:55:16 +0000

    fixed syntax in loop statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	printf("Last digit of %i is %i and is ", n, lastDigit);
+	printf("Last digit of %i is %i and is ", n, (n % 10));
 
 	if ((n % 10) > 5)
 		printf("greater than 5");
