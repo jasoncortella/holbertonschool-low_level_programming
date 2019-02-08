9ddfed5d85ebc629c1506f9eadb70431a2f44391
Jason Cortella <419@holbertonschool.com>
2019-02-08 00:48:04 +0000

    changed selection algorithm

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,18 +13,18 @@ int main(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			for (k = 0; k < 10; k++)
+			for (k = i; k < 10; k++)
 			{
 				for (l = 0; l < 10; l++)
 				{
-					if (((i + j) < (k + l) && i <= k) || ((i + j) == (k + l) && k > i))
+					if (i != k || j < l)
 					{
 						putchar(i + '0');
 						putchar(j + '0');
 						putchar(' ');
 						putchar(k + '0');
 						putchar(l + '0');
-						if ((i + j + k + l) < 36)
+						if ( ((i + j + k + l) < 35) || ( ((i + j + k + l) == 35) &&  i < j ))
 						{
 							putchar(',');
 							putchar(' ');
