4debe7cd9ae82f43214d67f969c07cc06a225cb5
Jason Cortella <419@holbertonschool.com>
2019-02-12 08:44:29 +0000

    first commit problem 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,15 +9,17 @@
 int main(void)
 {
 	int i;
-	long fib0 = 0, fib1 = 1, count = 0;
+	int fib0 = 0, fib1 = 1, count = 0;
 
 	while (fib1 < 4000000)
 	{
 		if (fib1 % 2 == 0)
-                        count += 0;
+                        count += fib1;
 		fib1 = fib1 + fib0;
 		fib0 = fib1 - fib0;
 	}
+
+	printf("%i\n", count);
 
 	return (0);
 }
