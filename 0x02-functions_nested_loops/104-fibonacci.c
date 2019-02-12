633aaf50f257f55d93ae112ed2cc8fb02f5ca182
Jason Cortella <419@holbertonschool.com>
2019-02-12 10:13:43 +0000

    added problem 16!

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,14 +11,36 @@ int main(void)
 	int i;
 
 	long fib1 = 1, fib2 = 2;
+	long split = 10000000000; /* long fails at fib 92, which has 20
+				   * digits. 'split' will be used to split
+				   * the number in half, digit wise
+				   */
 
 	printf("%lu", fib1);
 
-	for (i = 1; i < 98; i++)
+	for (i = 1; i < 90; i++) /* print the first 90 fibonacci terms.
+				  * this is the most that can be used with
+				  * out breaking the split functionality
+				  * below
+				  */
 	{
 		printf(", %lu", fib2);
 		fib2 = fib2 + fib1;
 		fib1 = fib2 - fib1;
+	}
+
+	long fib1a = fib1 / split;
+	long fib1b = fib1 % split;
+	long fib2a = fib2 / split;
+	long fib2b = fib2 % split;
+
+	for (i = 90; i < 98; i++)
+	{
+		printf(", %lu%lu", (fib2a + fib2b / split), (fib2b % split));
+		fib2a = fib2a + fib1a;
+		fib2b = fib2b + fib1b;
+		fib1a = fib2a - fib1a;
+		fib1b = fib2b - fib1b;
 	}
 
 	printf("\n");
