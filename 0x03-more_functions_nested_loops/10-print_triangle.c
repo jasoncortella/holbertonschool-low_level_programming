bf9250d23048018a1c83e41fcb9ac78713f95fc6
Jason Cortella <419@holbertonschool.com>
2019-02-14 22:05:43 +0000

    removed one extra space

--- 0x03-more_functions_nested_loops/10-print_triangle.c
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -14,7 +14,7 @@ void print_triangle(int size)
 		_putchar('\n');
 	for (i = 0; i < size; i++)
 	{
-		for (j = i; j < size; j++)
+		for (j = i; j < (size - 1); j++)
 			_putchar(' ');
 		for (j = 0; j <= i; j++)
 			_putchar('#');
