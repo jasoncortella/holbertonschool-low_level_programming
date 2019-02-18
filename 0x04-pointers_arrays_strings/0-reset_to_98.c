d4a455802d99e27829936f11a50a5021f934f74f
Jason Cortella <419@holbertonschool.com>
2019-02-18 22:40:55 +0000

    first commit problem 1

---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - updates the value a pointer to an int points to to 98
+ *@n: the pointer
+ *Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ *swap_int - swaps the values of two integers
+ *@a: the first integer to swap
+ *@b: the second integer to swap
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
