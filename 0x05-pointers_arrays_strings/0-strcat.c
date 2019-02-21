8546e94a98d565642470bf37810775195f7188c2
Jason Cortella <419@holbertonschool.com>
2019-02-21 18:16:25 +0000

    first commit p1

---
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ **_strcat - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *Return: the concatenated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	char *s = dest;
+	i = 0;
+	j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+
+	while (*(src + j) != '\0')
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+
+	*(dest + i) = '\0';
+
+	return (s);
+}
