a9bbce25a02f990f9d5d2d66a4e0200c2a779b4c
Jason Cortella <419@holbertonschool.com>
2019-02-21 21:34:30 +0000

    first commit p7

---
+++ 0x05-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ **cap_string - capitalizes all words of a string
+ *@str: the string to be converted
+ *Return: the converted string
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char *s = str;
+	char flags1[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char flags2[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		for (j = 0; j <= 10; j++)
+                {
+                        if (str[i] == flags1[j])
+                                str[i] = flags2[j];
+                }
+
+                i++;
+	}
+	return (s);
+}
