291978985353f780e12dc159815abb23c5b92b0f
Jason Cortella <419@holbertonschool.com>
2019-02-25 21:17:13 +0000

    first commit p5

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ **_strstr - locates a substring
+ *@haystack: the string to be searched
+ *@needle: the string to search for
+ *Return: (s) a pointer to the first matching instance, or 0 if no matches
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for (; haystack != '\0'; haystack++)
+	{
+		if (haystack[0] == needle[0])
+		{
+			i = 0;
+			while (needle[i] != '\0')
+			{
+				if (haystack[i] == needle[i])
+					i++;
+			}
+			if (needle[i] == '\0')
+				return (haystack);
+		}
+	}
+
+	return (NULL);
+}
