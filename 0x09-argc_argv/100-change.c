a80bb2b296a74a8121bb016af42903792eaaf610
Jason Cortella <419@holbertonschool.com>
2019-03-02 18:05:16 +0000

    reverted last commit

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -11,20 +11,18 @@ int main(int argc, char **argv)
 	int i, change, numcoins = 0, coins[5] = {25, 10, 5, 2, 1};
 
 	if (argc - 2)
-	{
 		printf("Error\n");
-		return (1);
-	}
-	change = atoi(argv[1]);
-	if (change < 0)
+	if (atoi(argv[argc - 1]) < 0)
 		printf("0\n");
-	for (i = 0; change > 0; i++)
+	if ((argc - 2) || (atoi(argv[1]) < 0))
+		return (1);
+	change = atoi(argv[1]);
+	for (i = 0; change; i++)
 		while (change >= coins[i])
 		{
 			change -= coins[i];
 			numcoins++;
 		}
-	if (numcoins > 0 )
-		printf("%i\n", numcoins);
+	printf("%i\n", numcoins);
 	return (0);
 }
