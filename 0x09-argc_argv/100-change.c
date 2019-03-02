c2903870a7a64205c1831fcc22f611f8f55c9667
Jason Cortella <419@holbertonschool.com>
2019-03-02 17:06:51 +0000

    now handles -1a as a negative instead of a symbol

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -15,17 +15,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+		return (1);
+	}
 	for (i = 0; argv[1][i]; i++)
 		if (argv[1][i] < '0' || argv[1][i] > '9')
 		{
 			printf("Error\n");
 			return (1);
 		}
-	if (atoi(argv[1]) < 0)
-	{
-		printf("0\n");
-		return (1);
-	}
 	change = atoi(argv[1]);
 	for (i = 0; change; i++)
 		while (change >= coins[i])
