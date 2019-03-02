9cfaba6fb5404f3d93ab423d713d45da68b31013
Jason Cortella <419@holbertonschool.com>
2019-03-02 18:01:12 +0000

    changed negatives to return 0

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -11,18 +11,20 @@ int main(int argc, char **argv)
 	int i, change, numcoins = 0, coins[5] = {25, 10, 5, 2, 1};
 
 	if (argc - 2)
+	{
 		printf("Error\n");
-	if (atoi(argv[argc - 1]) < 0)
-		printf("0\n");
-	if ((argc - 2) || (atoi(argv[1]) < 0))
 		return (1);
+	}
 	change = atoi(argv[1]);
-	for (i = 0; change; i++)
+	if (change < 0)
+		printf("0\n");
+	for (i = 0; change > 0; i++)
 		while (change >= coins[i])
 		{
 			change -= coins[i];
 			numcoins++;
 		}
-	printf("%i\n", numcoins);
+	if (numcoins > 0 )
+		printf("%i\n", numcoins);
 	return (0);
 }
