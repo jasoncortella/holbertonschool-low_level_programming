090d030c99a188cb2dd781282ba575e4a47fdb06
Jason Cortella <419@holbertonschool.com>
2019-03-02 17:29:07 +0000

    shortened code

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -11,22 +11,11 @@ int main(int argc, char *argv[])
 	int i, change, numcoins = 0, coins[5] = {25, 10, 5, 2, 1};
 
 	if (argc - 2)
-	{
 		printf("Error\n");
-		return (1);
-	}
-	if (atoi(argv[1]) < 0)
-	{
+	if (atoi(argv[argc - 1]) < 0)
 		printf("0\n");
+	if ((argc - 2) || (atoi(argv[1]) < 0))
 		return (1);
-	}
-/*	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] < '0' || argv[1][i] > '9')
-		{
-			printf("Error\n");
-			return (1);
-		}
-*/
 	change = atoi(argv[1]);
 	for (i = 0; change; i++)
 		while (change >= coins[i])
