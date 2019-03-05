8c0cef2e82aa5884f9e0caf676e7fda27e4fa427
Jason Cortella <419@holbertonschool.com>
2019-03-05 21:31:00 +0000

    changed for loop meshing

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -25,10 +25,9 @@ int **alloc_grid(int width, int height)
 		s[i] = malloc(width * sizeof(int));
 		if (!s[i])
 			return (NULL);
-	}
-
-	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			s[i][j] = 0;
+	}
+
 	return (s);
 }
