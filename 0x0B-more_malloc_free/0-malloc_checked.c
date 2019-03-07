0bc7343eeff42168b9df29477c373f0b2a1c81f4
Jason Cortella <419@holbertonschool.com>
2019-03-07 20:03:11 +0000

    betty formatting

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -3,8 +3,9 @@
 #include "holberton.h"
 
 /**
- * *malloc-checked - allocates memory using malloc
+ * *malloc_checked - allocates memory using malloc
  * @b: the amount of memory to allocate
+ * Return: a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
