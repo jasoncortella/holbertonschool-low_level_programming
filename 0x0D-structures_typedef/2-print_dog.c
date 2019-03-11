5670c871fccb5ac96d5bdfc4205eddc2dfd5688a
Jason Cortella <419@holbertonschool.com>
2019-03-11 20:08:26 +0000

    changed age ooutput

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		printf("Age: (nil)\n");
+		printf("Age: %s\n", d->age);
 		if (d->owner)
 			printf("Owner: %s\n", d->owner);
 		else
