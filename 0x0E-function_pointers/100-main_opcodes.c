be08130f9da18a649e301a97e2f748316aaf8f2a
Jason Cortella <419@holbertonschool.com>
2019-03-13 22:24:22 +0000

    initial commit

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of main
+ * @argc: the number of passed arguments
+ * @argv: an array of the passed arguments
+ * Return: 0 (success), 1 (incorrect number of arguments), 2 (argument < 0)
+ */
+
+int main(int argc, char *argv[])
+{
+	int a1, i;
+	char *mainpt = (char *)&main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	a1 = atoi(argv[1]);
+
+	if (a1 < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < (a1 - 1); i++)
+		printf("%hhx ", mainpt[i]);
+	i++;
+	printf("%hhx\n", mainpt[i]);
+
+	return (0);
+}
