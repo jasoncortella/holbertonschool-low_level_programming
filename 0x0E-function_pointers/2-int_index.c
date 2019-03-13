75350d8bbcc1271822308caa6ebd36123f50aa93
Jason Cortella <419@holbertonschool.com>
2019-03-13 20:54:57 +0000

    moved NULL check outside of loop

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -12,8 +12,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	for (i = 0; i < size; i++)
-		if (array[i] && cmp)
+	if (array && cmp)
+		for (i = 0; i < size; i++)
 			if (cmp(array[i]))
 				return (i);
 	return (-1);
