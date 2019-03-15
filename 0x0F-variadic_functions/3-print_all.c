c2d7b84ecb110110d65515eb9bfd2a67f9a9eb82
Jason Cortella <419@holbertonschool.com>
2019-03-15 22:01:03 +0000

    initial commit p3

---
+++ 0x0F-variadic_functions/3-print_all.c
@@ -0,0 +1,90 @@
+#include "variadic_functions.h"
+
+/**
+ * print_char - prints a character
+ * @c: the character to be printed
+ * Return: nothing
+ */
+void print_char(va_list valist)
+{
+	printf("%c", va_arg(valist, int));
+}
+
+/**
+ * print_int - prints an integer
+ * @i: the integer to be printed
+ * Return: nothing
+ */
+void print_int(va_list valist)
+{
+	printf("%i", va_arg(valist, int));
+}
+
+/**
+ * print_float - prints a floating point
+ * @f: the float to be printed
+ * Return: nothing
+ */
+void print_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+
+/**
+ * print_str - prints a string
+ * @s: the string to be printed
+ * Return: nothing
+ */
+void print_str(va_list valist)
+{
+	char *cpy = va_arg(valist, char *);
+
+	if (cpy)
+	{
+		printf("%s", cpy);
+		return;
+	}
+	printf("(nil)");
+}
+
+/**
+ * print_all - prints anything, by calling other functions
+ * @format: a string containing the cariable types passed in to be printed
+ * Return: nothing
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list valist;
+	unsigned int i = 0, j;
+	char *separator = "";
+	print_t p[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_str},
+		{NULL, NULL}
+	};
+
+	va_start(valist, format);
+
+	while (format[i])
+	{
+		j = 0;
+		while (p[j].type)
+		{
+			if (*p[j].type == format[i])
+			{
+				printf("%s", separator);
+				p[j].func(valist);
+				separator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+
+	va_end(valist);
+
+	printf("\n");
+}
