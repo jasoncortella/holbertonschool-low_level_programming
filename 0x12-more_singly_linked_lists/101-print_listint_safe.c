df68fa86e2b2f554428b8c28baca1c3ae4041f5b
Jason Cortella <419@holbertonschool.com>
2019-03-26 18:49:11 +0000

    Initial commit p12

---
+++ 0x12-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints all the elements of a listint_t list
+ * @head: the input listint_t list
+ *
+ * Return: the number of nodes.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	uint i = 1, j;
+	const listint_t *tmp = head, *check;
+
+	for (; tmp; i++)
+	{
+		printf("[%p] %d\n", (void *)tmp, tmp->n);
+		tmp = tmp->next;
+		check = head;
+		for (j = 0; j < i; j++)
+		{
+			if (tmp == check)
+			{
+				printf("-> [%p] %d\n", (void *)tmp, tmp->n);
+				return (i);
+			}
+			check = check->next;
+		}
+	}
+	return (i);
+}
