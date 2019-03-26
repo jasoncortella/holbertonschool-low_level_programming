ac28f6c19522fb77698491e01df969c5e7309b1d
Jason Cortella <419@holbertonschool.com>
2019-03-26 16:43:17 +0000

    Added empty head coverage

--- 0x12-more_singly_linked_lists/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * rverse_listint - reverses a listint_t linked list
+ * reverse_listint - reverses a listint_t linked list
  * @head: the input listint_t list
  *
  * Return: a pointer to the first node of the reversed list
@@ -11,6 +11,8 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *current, *tmp;
 
+	if (!head || !*head)
+		return (NULL);
 	current = *head;
 	while (current->next)
 	{
