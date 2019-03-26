8ed1547dffa5fa06b7dbeff0c1e25e985ca02b53
Jason Cortella <419@holbertonschool.com>
2019-03-26 16:15:30 +0000

    Add coverage for idx < 0

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,7 +13,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *tmp, *newNode;
 
-	if (!head)
+	if (!head || idx < 0)
 		return (NULL);
 	tmp = *head;
 	for (; idx - 1; idx--)
