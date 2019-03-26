e4a2ee992afb198421d14d06019150ad81b5f2f3
Jason Cortella <419@holbertonschool.com>
2019-03-26 16:11:11 +0000

    Add coverage for NULL head

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * inesert_nodeint_at_index - inserts a new node at a given position
+ * insert_nodeint_at_index - inserts a new node at a given position
  * @head: the input listint_t list
  * @idx: the index of the list where the new node should be added
  * @n: the value of the new node's int
@@ -13,6 +13,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *tmp, *newNode;
 
+	if (!head)
+		return (NULL);
 	tmp = *head;
 	for (; idx - 1; idx--)
 	{
