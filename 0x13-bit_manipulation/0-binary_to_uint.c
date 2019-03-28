6adfa100838027196786a5c628dfdbe663f54a11
Jason Cortella <419@holbertonschool.com>
2019-03-28 19:06:20 +0000

    Format compliant to betty standard

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -17,7 +17,7 @@ unsigned int binary_to_uint(const char *b)
 	for (i = 0; b[i]; i++)
 		if (b[i] != '1' && b[i] != '0')
 			return (0);
-	for (; i; i--, factor *=2)
+	for (; i; i--, factor *= 2)
 		sum += (b[i - 1] - '0') * factor;
 	return (sum);
 }
