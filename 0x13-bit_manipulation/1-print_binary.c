762dcd3a395bd13de1f3c06f252cb03f545bd868
Jason Cortella <419@holbertonschool.com>
2019-03-28 17:14:35 +0000

    Initial commit p1

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: the input integer
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar(n + '0');
+}
