9409060a21d2813191191fc16160877dc0dd6711
Jason Cortella <419@holbertonschool.com>
2019-04-02 22:55:23 +0000

    Swap out creat for read

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -4,24 +4,24 @@
  * main - copies contents of one file to another file
  * @argc: the number of input arguments
  * @argv: a character array containing the input arguments
- * Return: (1) On success
+ * Return: (0) On success
  */
 
 int main(int argc, char *argv[])
 {
 	int fd1, fd2, ret, wrcheck;
-	char buf[BUFSIZE];
+	char buf[BUFSIZ];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd1 = open(argv[1], O_RDONLY, 0);
+	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
-	fd2 = creat(argv[2], 0666);
+	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while ((ret = read(fd1, buf, BUFSIZE)) > 0)
+	while ((ret = read(fd1, buf, BUFSIZ)) > 0)
 	{
 		wrcheck = (write(fd2, buf, ret));
 		if (wrcheck != ret)
