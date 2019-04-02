d99d04e96dcdc3626dc4fad2e2dc989e9258f9ad
Jason Cortella <419@holbertonschool.com>
2019-04-02 23:20:42 +0000

    Shorten amont of lines

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -9,32 +9,21 @@
 
 int main(int argc, char *argv[])
 {
-	int fd1, fd2, ret, wrcheck;
+	int fd1, fd2, ret, ret2;
 	char buf[BUFSIZ];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd1 = open(argv[1], O_RDONLY);
-	if (fd1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-			exit(98);
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	while ((ret = read(fd1, buf, BUFSIZ)) > 0)
-	{
-		wrcheck = (write(fd2, buf, ret));
-		if (wrcheck != ret)
+		if (fd2 == -1 || write(fd2, buf, ret) != ret)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	}
-	if (ret == -1)
+	if (fd1 == -1 || ret == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 			exit(98);
-	ret = close(fd1);
-	if (ret == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1), exit(100);
-	ret = close(fd2);
-	if (ret == -1)
+	ret = close(fd1), ret2 = close(fd2);
+	if (ret == -1 || ret2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %d\n", fd2), exit(100);
 	return (0);
 }
