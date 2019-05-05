ceb50948cdf64ca6b61cbcdc98e0cdd762b787c2
Jason Cortella <419@holbertonschool.com>
2019-05-05 05:18:10 +0000

    Condense code

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,32 +9,17 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *current = *h;
+	dlistint_t *new =  malloc(sizeof(dlistint_t)), *current = *h;
 	unsigned int i = 0;
 
 	for (; i < idx - 1 && current; i++)
 		current = current->next;
 	if (current || idx == 0 || i == idx - 1)
 	{
-		new = malloc(sizeof(dlistint_t));
-		if (!new)
-			return (NULL);
 		new->n = n;
-		if (idx == 0)
-		{
-			new->next = *h;
-			new->prev = NULL;
-			*h = new;
-		}
-		else
-		{
-			new->next = current->next;
-			new->prev = current;
-			current->next = new;
-		}
-/*		new->next = idx ? current->next : *h; */
-/*		new->prev = idx ? current : NULL; */
-/*		idx ? (current->next = new) : (*h = new); */
+		new->next = idx ? current->next : *h;
+		new->prev = idx ? current : NULL;
+		idx ? (current->next = new) : (*h = new);
 		if (new->next)
 			new->next->prev = new;
 		return (new);
