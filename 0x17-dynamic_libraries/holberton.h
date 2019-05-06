a6c5050c3968110198cd5049447a3e80c9965471
Jason Cortella <419@holbertonschool.com>
2019-05-06 18:27:37 +0000

    Initial commit p0, p1, p3

--- 0x17-dynamic_libraries/holberton.h
+++ 0x17-dynamic_libraries/holberton.h
@@ -1,6 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdlib.h>
+#include <string.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -15,6 +18,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+int _strncmp(char *s1, char *s2, int idx);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
