5ebd30ec6f823cc5a2de33f5a38c97b4a902692f
Jason Cortella <419@holbertonschool.com>
2019-06-04 01:56:22 +0000

    Add check for value == 0

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *current, *newNode;
 
-	if (!ht || !key || !*key)
+	if (!ht || !key || !*key || !value)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
