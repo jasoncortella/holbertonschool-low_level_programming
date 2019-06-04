7c58dfde9d3482d6bb72eb766fcc4d97202d42d9
Jason Cortella <419@holbertonschool.com>
2019-06-04 02:04:07 +0000

    Change malloc statement to node

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -22,9 +22,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (current) /* Match found, update value and return */
 	{
 		current->value = strdup(value);
+		if (!current->value)
+			return (0);
 		return (1);
 	}
-	newNode = malloc(sizeof(hash_table_t));
+	newNode = malloc(sizeof(hash_node_t));
 	if (!newNode)
 		return (0);
 	newNode->key = strdup(key);
