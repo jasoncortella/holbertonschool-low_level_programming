7304f03899408ee89bc55ebdfd363ec9fdc58b66
Jason Cortella <419@holbertonschool.com>
2019-06-04 02:17:25 +0000

    Improve list building structure

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -12,37 +12,41 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *current, *newNode;
+	char *value_copy, *key_copy;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
+
 	current = ht->array[index];
+	value_copy = strdup(value);
+	if (!value_copy)
+		return (0);
+	key_copy = strdup(key);
+	if (!key_copy)
+	{
+		free(value_copy);
+		return (0);
+	}
+
 	while (current && strcmp(current->key, key) != 0)
 		current = current->next;
 	if (current) /* Match found, update value and return */
 	{
-		current->value = strdup(value);
-		if (!current->value)
-			return (0);
+		current->value = value_copy;
 		return (1);
 	}
+
 	newNode = malloc(sizeof(hash_node_t));
 	if (!newNode)
-		return (0);
-	newNode->key = strdup(key);
-	if (!newNode->key)
 	{
-		free(newNode);
+		free(value_copy);
+		free(key_copy);
 		return (0);
 	}
-	newNode->value = strdup(value);
-	if (!newNode->value)
-	{
-		free(newNode->key);
-		free(newNode);
-		return (0);
-	}
-	newNode->next = current;
+	newNode->key = key_copy;
+	newNode->value = value_copy;
+	newNode->next = ht->array[index];
 	ht->array[index] = newNode;
 	return (1);
 }
