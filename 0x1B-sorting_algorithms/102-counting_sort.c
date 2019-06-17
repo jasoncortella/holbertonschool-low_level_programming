a4f57e49bb727a38bbce9c36722e1ebb47078365
Jason Cortella <419@holbertonschool.com>
2019-06-17 21:57:06 +0000

    Initial commit p6

---
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * counting_sort - sorts an array of integers in ascending order using
+ *               the Counting sort algorithm
+ * @array: Array to sort
+ * @size: Size of the array
+ */
+void counting_sort(int *array, size_t size)
+{
+	int *countarray, index, instances;
+	size_t i, maxval = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > (int)maxval)
+			maxval = array[i];
+	}
+	countarray = calloc(maxval, sizeof(int));
+	for (i = 0; i < size; i++)
+	{
+		index = array[i];
+		countarray[index] += 1;
+	}
+	for (i = 1; i <= maxval; i++)
+		countarray[i] += countarray[i - 1];
+	print_array(countarray + 1, maxval);
+
+	for (i = 0; i <= maxval; i++)
+		if (countarray[i] != countarray[i + 1] && countarray[i + 1])
+		{
+			instances = countarray[i + 1] - countarray[i];
+			while (instances)
+			{
+				array[countarray[i] + instances - 1] = i + 1;
+				instances--;
+			}
+		}
+	free(countarray);
+}
