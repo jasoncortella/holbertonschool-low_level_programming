4a72000b953e64552d3a5ed01f9b7a3c4096a375
Jason Cortella <419@holbertonschool.com>
2019-07-11 21:14:07 +0000

    remove import numpy

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 """ Function to find an island's perimeter"""
 
-import numpy as np
-
 def island_perimeter(grid):
     """Find perimeter"""
     pgrid = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
-#    print("pgrid initialization")
-#    print(np.matrix(pgrid))
-#    print("---------------")
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
@@ -24,9 +19,4 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             perimeter += pgrid[i][j]
-#    print("grid")
-#    print(np.matrix(grid))
-#    print("---------------")
-#    print("pgrid")
-#    print(np.matrix(pgrid))
     return perimeter
