2b3bc8a2132b37e039241a61a3e18098fc6aea2e
Jason Cortella <419@holbertonschool.com>
2019-07-11 21:28:55 +0000

    pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,30 +3,31 @@
 
 # numpy as np
 
+
 def island_perimeter(grid):
     """Find perimeter"""
-    pgrid = [[0 for i in range(2 + len(grid[0]))] for j in range(2 + len(grid))]
+    pg = [[0 for i in range(2 + len(grid[0]))] for j in range(2 + len(grid))]
 #    print("pgrid initialization")
 #    print(np.matrix(pgrid))
 #    print("---------------")
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
-                pgrid[i+1][j] += 1  # above
-                pgrid[i+1][j+2] += 1  # below
-                pgrid[i][j+1] += 1  # left
-                pgrid[i+2][j+1] += 1  # right
+                pg[i+1][j] += 1  # above
+                pg[i+1][j+2] += 1  # below
+                pg[i][j+1] += 1  # left
+                pg[i+2][j+1] += 1  # right
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
-                pgrid[i+1][j+1] = 0
+                pg[i+1][j+1] = 0
     perimeter = 0
 #    print("grid")
 #    print(np.matrix(grid))
 #    print("---------------")
 #    print("pgrid")
 #    print(np.matrix(pgrid))
-    for i in range(len(pgrid)):
-        for j in range(len(pgrid[0])):
-            perimeter += pgrid[i][j]
+    for i in range(len(pg)):
+        for j in range(len(pg[0])):
+            perimeter += pg[i][j]
     return perimeter
