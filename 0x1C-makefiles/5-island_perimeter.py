d1f29b10c8ee7cf4c5d884564799901068839cb3
Jason Cortella <419@holbertonschool.com>
2019-07-11 21:15:55 +0000

    pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ Function to find an island's perimeter"""
 
+
 def island_perimeter(grid):
     """Find perimeter"""
     pgrid = [[0 for i in range(len(grid[0]))] for j in range(len(grid))]
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
-                pgrid[i][j-1] += 1 #above
-                pgrid[i][j+1] += 1 #below
-                pgrid[i-1][j] += 1 #left
-                pgrid[i+1][j] += 1 #right
+                pgrid[i][j-1] += 1  # above
+                pgrid[i][j+1] += 1  # below
+                pgrid[i-1][j] += 1  # left
+                pgrid[i+1][j] += 1  # right
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
