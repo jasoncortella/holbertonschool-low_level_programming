275efcd79e8bb21b6623e221a7b7f269510a46dd
Jason Cortella <419@holbertonschool.com>
2019-07-11 21:55:02 +0000

    Refactor, remove degug prints

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 """ Function to find an island's perimeter"""
 
-# impor t numpy as np
-
 
 def island_perimeter(grid):
-    """Find perimeter"""
+    """Find perimeter, note pg <-> perimeter grid"""
     pg = [[0 for i in range(2 + len(grid[0]))] for j in range(2 + len(grid))]
-#    print("pgrid initialization")
-#    print(np.matrix(pgrid))
-#    print("---------------")
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j]:
@@ -21,13 +16,4 @@ def island_perimeter(grid):
         for j in range(len(grid[0])):
             if grid[i][j]:
                 pg[i+1][j+1] = 0
-    perimeter = 0
-#    print("grid")
-#    print(np.matrix(grid))
-#    print("---------------")
-#    print("pgrid")
-#    print(np.matrix(pgrid))
-    for i in range(len(pg)):
-        for j in range(len(pg[0])):
-            perimeter += pg[i][j]
-    return perimeter
+    return sum(sum(pg,[]))
